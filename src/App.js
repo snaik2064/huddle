70d9ea38752f2c1a6ff07ffdd5275fcdd0c61e2c
Shashank <snaik2064@gmail.com>
2022-03-12 12:48:20 +0530

    [Feat]: Styled component basic theme & global styled created.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { ThemeProvider } from 'styled-components';
+
+import Header from './components/Header';
+import Container from './components/styles/Container.styled';
+import GlobalStyles from './components/styles/Global';
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+};
+
 function App() {
-  return <div className="App">In new app</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          <h1> In new app!!! </h1>{' '}
+        </Container>
+      </>
+    </ThemeProvider>
+  );
 }
 
 export default App;
